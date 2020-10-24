d2c1375c9bcd9fd69626605badd40a9748f683e4
Szilveszter Cserhati <cserhatiszilveszter1352@gmail.com>
2020-10-24 20:13:47 +0200

    Add project files.

---
+++ Models/Cocktail.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailsApi.Models
+{
+    public class Cocktail
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public IEnumerable<string> Ingredients { get; set; }
+        public IEnumerable<string> Steps { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/CocktailDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailsApi.Models
+{
+    public class CocktailDatabaseSettings : ICocktailDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ICocktailDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
